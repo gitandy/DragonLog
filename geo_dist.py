2f0cb395079fdcc61a3ab38a15b2abfae340d469
Andreas Schawo <andreas@schawo.de>
2023-10-08 15:50:17 +0200

    Removed unused module geo_dist

--- geo_dist.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def geo_dist(pos1, pos2):
-    mlat = math.radians(pos1[0])
-    mlon = math.radians(pos1[1])
-    plat = math.radians(pos2[0])
-    plon = math.radians(pos2[1])
-    return 6371.01 * math.acos(math.sin(mlat)*math.sin(plat) + math.cos(mlat)*math.cos(plat)*math.cos(mlon - plon))
