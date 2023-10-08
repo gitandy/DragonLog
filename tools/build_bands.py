16a689673f717f68fc902d60d4a355da6156fe83
Andreas Schawo <andreas@schawo.de>
2023-10-08 16:01:01 +0200

    Fixed sorting of bands

--- tools/build_bands.py
+++ tools/build_bands.py
@@ -17,12 +17,14 @@ def build_bands(src, dst):
                 continue
 
             fb = float(b[1])*1000
-            fe = float(b[1])*1000
+            fe = float(b[2])*1000
             fs = 1 if fb < 1000 else 100
 
-            bands[b[0].strip()] = [fb, fe, fs]
+            bands[b[0].strip()] = [int(fb) if fb > 1000 else fb, int(fe) if fe > 1000 else fe, fs]
 
     bands['11m'] = [26565, 27405, 10]
+
+    bands = dict(sorted(bands.items(), key=lambda item: item[1][0]))
 
     with open(dst, 'w') as bjf:
         print('Writing bands...')
