83a22072ae43047b6e706a4a90de7dcf4a9aa15a
Andreas Schawo <andreas@schawo.de>
2025-02-28 19:38:12 +0100

    Added more random content to adi file generator and adx

--- tools/gen_big_adi.py
+++ tools/gen_big_adi.py
@@ -1,66 +1,126 @@
+import os
 import sys
+import json
+import random
 import string
 import datetime
 
 from adif_file import adx, adi
 
+with open('../dragonlog/data/bands.json') as bf:
+    BANDS: dict = json.load(bf)
+    BANDS.pop('11m')
 
-def gen_call(calls=None, prefix='', suffix=''):
+with open('../dragonlog/data/modes.json') as mf:
+    MODES = json.load(mf)['AFU']
+
+
+def gen_call(calls: int = 1000, cnt_prefix: str = '', anc_suffix: str = ''):
     gen_calls = 0
-    for fpc in string.ascii_uppercase:
-        for spc in string.ascii_uppercase:
-            for nr in range(10):
-                for fsc in string.ascii_uppercase:
-                    for ssc in string.ascii_uppercase:
+    cnt_prefix = cnt_prefix.upper() + '/' if cnt_prefix else ''
+    anc_suffix = '/' + anc_suffix.upper() if anc_suffix else ''
+    for fpc in string.ascii_uppercase:  # First prefix char
+        for spc in string.ascii_uppercase:  # Second prefix char
+            for nr in range(10):  # Number
+                for fsc in string.ascii_uppercase:  # First suffix char
+                    for ssc in string.ascii_uppercase:  # Second suffix char
                         gen_calls += 1
                         if gen_calls > calls:
                             return
                         else:
-                            yield f'{prefix}{fpc}{spc}{nr}{fsc}{ssc}{suffix}'
+                            yield f'{cnt_prefix}{fpc}{spc}{nr}{fsc}{ssc}{anc_suffix}'
 
 
+def get_random_int(start: int, stop: int) -> int:
+    return int(random.random() * ((stop + 1) - start) + start)
+
+
+def get_rand_freq(band: str) -> float:
+    start, stop, _ = BANDS[band]
+    return get_random_int(start, stop) / 1000
+
+
+def get_rand_rst() -> str:
+    r = get_random_int(1, 5)
+    s = get_random_int(1, 9)
+    return f'{r}{s}'
+
+
+def get_rand_loc() -> str:
+    # A..R, 0..9, a..x
+    l1 = string.ascii_uppercase[get_random_int(0, 17)]
+    l2 = string.ascii_uppercase[get_random_int(0, 17)]
+    l34 = f'{get_random_int(0, 99):02d}'
+    l5 = string.ascii_lowercase[get_random_int(0, 24)]
+    l6 = string.ascii_lowercase[get_random_int(0, 24)]
+    return l1 + l2 + l34 + l5 + l6
+
+
+def get_rand_suffix() -> str:
+    suffixes = ('', '', '', '', '/M', '/P', '/T', '/AM', '/MM')
+    return suffixes[get_random_int(0, len(suffixes) - 1)]
+
+
+# noinspection PyPep8Naming
 def main():
     rec_amount = 1000
+    adif_type = 'i'
     if len(sys.argv) > 1:
         try:
             rec_amount = int(sys.argv[1])
         except ValueError:
             pass
 
+        if len(sys.argv) > 2 and sys.argv[2] == 'x':
+            adif_type = 'x'
+
     doc = {
-        'HEADER': {'ADIF_VER': '3.1.4',
-                   'CREATED_TIMESTAMP': datetime.datetime.now(datetime.UTC).strftime('%Y%m%d %H%M%S'),
-                   'PROGRAMID': 'Generate big test file',
-                   'PROGRAMVERSION': '0.1'},
+        'HEADER': {'CREATED_TIMESTAMP': datetime.datetime.utcnow().strftime('%Y%m%d %H%M%S'),
+                   'PROGRAMID': os.path.basename(sys.argv[0]),
+                   'PROGRAMVERSION': '0.2'},
         'RECORDS': []
     }
 
+    bands = list(BANDS.keys())
+    modes = list(MODES.keys())
+
     for i, call in enumerate(gen_call(rec_amount)):
-        record = {'CALL': call,
-                  'QSO_DATE': f'20231204',
-                  'TIME_ON': '1100',
-                  'TIME_OFF': '1105',
+        rY = get_random_int(2018, 2025)
+        rM = get_random_int(1, 12)
+        rD = get_random_int(1, 28) if rM == 2 else get_random_int(1, 30)
+        rh = get_random_int(0, 23)
+        rm = get_random_int(0, 54)
+        rb = bands[get_random_int(0, len(BANDS) - 1)]
+        rf = get_rand_freq(rb)
+
+        record = {'CALL': call + get_rand_suffix(),
+                  'QSO_DATE': f'{rY}{rM:02d}{rD:02d}',
+                  'TIME_ON': f'{rh:02d}{rm:02d}',
+                  'TIME_OFF': f'{rh:02d}{rm + 5:02d}',
                   'NAME': f'Test OM #{i}',
                   'QTH': f'Test #{i}',
                   'NOTES': f'Test file #{i}',
                   'STATION_CALLSIGN': 'XX1XXX',
-                  'GRIDSQUARE': 'JO33uu',
-                  'RST_SENT': '59',
-                  'RST_RCVD': '59',
-                  'BAND': '10m',
-                  'MODE': 'SSB',
+                  'GRIDSQUARE': get_rand_loc(),
+                  'RST_SENT': get_rand_rst(),
+                  'RST_RCVD': get_rand_rst(),
+                  'BAND': rb,
+                  'MODE': modes[get_random_int(0, len(modes) - 1)],
                   'SUBMODE': 'USB',
-                  'FREQ': 28400,
-                  'TX_PWR': 50,
-                  'MY_NAME': 'Paul',
-                  'MY_CITY_INTL': 'Very large city name',
+                  'FREQ': rf,
+                  'TX_PWR': get_random_int(1, 100),
+                  'MY_NAME': 'Paul Mustermann',
+                  'MY_CITY': 'Very large city name',
                   'MY_GRIDSQUARE': 'JO77zz',
                   }
 
         doc['RECORDS'].append(record)
 
     print(f'Generating testfile for {rec_amount} QSOs...')
-    adi.dump(f'big_testfile_{rec_amount}.adi', doc)
+    if adif_type == 'i':
+        adi.dump(f'big_testfile_{rec_amount}.adi', doc, comment=f'Big test file with {rec_amount} QSOs')
+    else:
+        adx.dump(f'big_testfile_{rec_amount}.adx', doc, False)
 
 
 if __name__ == '__main__':
